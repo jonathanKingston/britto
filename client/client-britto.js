5f788c696d1efdf09d1f2bb8921340d60b5fc8f0
Jonathan Kingston <jonathan@jooped.co.uk>
2012-04-22 18:35:08 +0100

    adding seperate login page

--- client/client-britto.js
+++ client/client-britto.js
@@ -22,8 +22,8 @@
     return Comments.find({postId: id}).count();
   }
 
-  Template.postView.postUser = function(post) {
-    return Users.findOne({_id: post.userId}).name;
+  Template.post.postUser = function(id) {
+    return Users.findOne({_id: id}).name;
   }
 
   Template.comments.commentslist = function(post) {
@@ -36,17 +36,18 @@
   }
 
   Template.userArea.events = {
-    'submit #login-form, click #login-button': function() {
+    'submit #login-button, click #login-button': function() {
       Meteor.call('login', $('#login-username').val(), $('#login-password').val(), loginCallback);
       return false;
     },
-    'submit #post-form, click #post-button': function() {
+    'submit #post-button, click #post-button': function() {
       Meteor.call('post', {title: $('#post-title').val(), body: $('#post-body').val(), slug: $('#post-slug').val(), auth: Session.get('auth')});
       return false;
     }
   }
+
   Template.postView.events = {
-    'submit #comment-form, click #comment-button': function() {
+    'submit #comment-button, click #comment-button': function() {
       Meteor.call('comment', {name: $('#comment-name').val(), comment: $('#comment-comment').val(), postId: $('#comment-post').val()});
       return false;
     }
@@ -78,26 +79,32 @@
 
   Handlebars.registerHelper('content', function() {
     if(Session.equals('loaded', true)) {
-    if(Session.equals('new_page', 'post')) {
-      post = Posts.findOne({slug: Session.get('new_slug')});
-      if(post) {
-//        Meteor.subscribe("postcomments", post._id, init);
-        return Meteor.ui.chunk(function() { return Template.postView({post: post}); });
+      if(Session.equals('new_page', 'post')) {
+        post = Posts.findOne({slug: Session.get('new_slug')});
+        if(post) {
+  //TODO        Meteor.subscribe("postcomments", post._id, init);
+          return Meteor.ui.chunk(function() { return Template.postView({post: post}); });
+        }
+      } else if(Session.equals('new_page', 'userArea')) {
+        return Meteor.ui.chunk(function() { return Template.userArea(); });
       }
-    }
-    return Meteor.ui.chunk(function() { return Template.listView(); });
+      return Meteor.ui.chunk(function() { return Template.listView(); });
     }
     return '';
   });
 
   BrittoRouter = Backbone.Router.extend({
     routes: {
+      "user-area/": "userAreaPage",
       ":slug": "findPost",
       ":slug/": "findPost"
     },
     findPost: function(slug) {
       Session.set('new_page', 'post');
       Session.set('new_slug', slug);
+    },
+    userAreaPage: function() {
+      Session.set('new_page', 'userArea');
     }
   });
   Router = new BrittoRouter;
