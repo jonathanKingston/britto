0920753fc476231c5a71f266cb084717ef1388f3
Jonathan Kingston <jonathan@jooped.co.uk>
2012-04-25 03:21:46 +0100

    Adding in changes to disqus, still doesn't work

--- client/js/very-good.js
+++ client/js/very-good.js
@@ -10,20 +10,40 @@
 
   function init() {
     loadDisqus();
+    loadAnalytics();
     Session.set('loaded', true);
     Backbone.history.start({pushState: true});
+  }
+
+  function loadAnalytics() {
+    analytics = Settings.findOne({key: 'analytics_code'});
+    if(analytics && analytics.value != '') {
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', analytics.value]);
+//      _gaq.push(['_setDomainName', 'britto.co']);
+      _gaq.push(['_setAllowLinker', true]);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    }
   }
 
   function loadDisqus() {
     disqus = Settings.findOne({key: 'disqus'});
     if(disqus && disqus.value != '') {
-      var disqus_shortname = disqus.value;
-      /* * * DON'T EDIT BELOW THIS LINE * * */
-      (function() {
-          var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-          dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-          (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-      })();
+      $(document).load(function() {
+        var disqus_shortname = disqus.value;
+        (function() {
+            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+            dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        })();
+      });
     }
   }
 
