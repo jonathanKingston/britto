7a98538610e835724fd9329fbc99fe87816ced76
Jonathan Kingston <jonathan@jooped.co.uk>
2012-04-27 13:54:38 +0100

    Adding analytics code - fixed :)

--- client/js/britto.js
+++ client/js/britto.js
@@ -61,18 +61,7 @@ Britto.load = {};
 Britto.load.analytics = function() {
   analytics = Settings.findOne({key: 'analytics_code'});
   if(analytics && analytics.value != '') {
-    var _gaq = _gaq || [];
-    Britto.analytics = _gaq;
-    _gaq.push(['_setAccount', analytics.value]);
-// Not sure if these are needed
-//    _gaq.push(['_setDomainName', window.location.host]);
-//    _gaq.push(['_setAllowLinker', true]);
-    _gaq.push(['_trackPageview']);
-   (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
+    $.ga.load(analytics.value);
   }
 }
 
