2674b55300dfc71ad5e2ab23cd1f67a82cddb734
Jonathan Kingston <jonathan@mobileKingUnit.(none)>
2012-05-12 14:00:50 +0100

    Fix login init

--- server/startup.js
+++ server/startup.js
@@ -9,10 +9,16 @@ Meteor.startup(function () {
   if(Users.find().count() === 0) {
     console.log('Adding in users');
     userId = createUser({username: 'admin', password: 'password', name: 'Your name'});
+    console.log('Logging in test user');
+    var user = loginUser('admin', 'password');
+    var key = user.auth;
     console.log('Adding in test post');
-    user = Users.findOne({_id: userId});
-    makePost({title: 'Hello world', body: 'Cruel cruel world', slug: 'yellow_world', id: user._id});
+    makePost({title: 'Hello world', body: 'Cruel cruel world', slug: 'yellow_world', auth: key});
+    console.log('Adding in test post');
     //Make a long post now
-    makePost({title: 'White Riot', body: "White riot - a riot of my own\nWhite riot - I want to riot\nWhite riot - a riot of my own\n\nBlack people gotta lot a problems\nBut they don't mind throwing a brick\nWhite people go to school\nWhere they teach you how to be thick\n\nAn' everybody's doing\nJust what they're told to\nAn' nobody wants\nTo go to jail!\n\n\nll the power's in the hands\nOf people rich enough to buy it\nWhile we walk the street\nToo chicken to even try it\n\nEverybody's doing\nJust what they're told to\nNobody wants\nTo go to jail!\n\nAre you taking over\nOr are you taking orders?\nAre you going backwards\nOr are you going forwards?", slug: 'white_riot', user: user._id});
+    console.log('Adding in test post2');
+    makePost({title: 'White Riot', body: "White riot - a riot of my own\nWhite riot - I want to riot\nWhite riot - a riot of my own\n\nBlack people gotta lot a problems\nBut they don't mind throwing a brick\nWhite people go to school\nWhere they teach you how to be thick\n\nAn' everybody's doing\nJust what they're told to\nAn' nobody wants\nTo go to jail!\n\n\nll the power's in the hands\nOf people rich enough to buy it\nWhile we walk the street\nToo chicken to even try it\n\nEverybody's doing\nJust what they're told to\nNobody wants\nTo go to jail!\n\nAre you taking over\nOr are you taking orders?\nAre you going backwards\nOr are you going forwards?", slug: 'white_riot', auth: key});
+    console.log('Log out in test user');
+    logoutSession(key);
   }
 });
